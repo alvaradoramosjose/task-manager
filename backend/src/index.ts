de36e548c0eced85184974802cf16cbf83b4ff83
Juan Jose Alvarado Ramos <alvarado.ramos.jose@gmail.com>
2026-09-19 10:00:05 -0400

    feat: agregar deploy staging

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,6 +15,8 @@ app.use(express.json());
 
 const SECRET_KEY = "secret_key";
 
+const apiKey = process.env.API_KEY || 1234;
+
 const authenticateToken = (req: any, res: any, next: any) => {
     const authHeader = req.headers.authorization;
     if (!authHeader) {
